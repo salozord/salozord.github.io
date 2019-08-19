2cc29f7063fafe6dbd48a7e89a6b8099d71c1689
Santiago Salazar <s.salazara@uniandes.edu.co>
2019-08-19 00:56:20 -0500

    Using JS for changing pictures.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 console.log('is working!');
+window.onload = () => {
+
+    var path = './assets/img/';
+    var images = ['op1.jpg', 'op2.jpg', 'op3.jpg'];
+    
+    var num = 1;
+    function changePic() {
+        let pic = document.getElementById('profile-pic');
+        let newpic = document.createElement('img');
+        newpic.src = path + images[num++];
+        newpic.classList = pic.classList;
+        newpic.id = pic.id;
+        newpic.alt = pic.alt;
+        
+        if (num === 3) {
+            num = 0;
+        }
+        //pic.classList.remove('fadingIn');
+        pic.classList.add('fadingOut');
+        let dad = pic.parentNode;
+        setTimeout(()=> {
+            dad.removeChild(pic);
+            dad.prepend(newpic);
+        },990);
+    }
+    setInterval(changePic, 5000);
+};
