28f640c8166ee206f40651e45bcb642715f22f38
Santiago Salazar <s.salazara@uniandes.edu.co>
2019-08-19 23:31:39 -0500

    FINISHED !

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Personal WebPage
 ¡Hi! Mi name is Santiago Salazar. The web page that you will find in this repository pretends to talk about me, apart from other stuff related with my studies, preferences, personal projects and much more.
-If you want to have mi CV, you can find it in the following link:
-ADD LINK
 
 ## Libraries or External Code/Elements
 The following libraries or external code/elements were used apart from my own work during the development of this webpage:
     * Fontawesome icons free [visit their site](https://fontawesome.com/)
+    * Radio D3.js Spider Chart [visit the site](http://bl.ocks.org/nbremer/21746a9668ffdf6d8242)
 
 ## Page's Domain (URL)
 The page is live, runing in the URL [santiagosalazar.me](https://santiagosalazar.me) which is pointing to this repository on github. This means that accessing to the URL [salozord.github.io](https://salozord.github.io) will redirect successfully to the domain mentioned above. 
--- js/main.js
+++ js/main.js
@@ -22,11 +22,57 @@ window.onload = () => {
         setTimeout(()=> {
             dad.removeChild(pic);
             dad.prepend(newpic);
-        },960);
+        }, 960);
     }
     setInterval(changePic, 5000);
 
-   $('.nav-link').on('click',function() {
+    $('.nav-link').on('click',function() {
         $('.navbar-collapse').collapse('hide');
     });
 };
+
+let m = (window.innerWidth <= 800) ? 80 : 100;
+let width = (window.innerWidth <= 800) ? (window.innerWidth - m*2) - 20 : (window.innerWidth - m*2)/3 - 20,
+	height = Math.min(width, window.innerHeight - m*2 - 20);
+let data = [[
+      {axis:"Android",value:0.34},
+      {axis:"Angular 8",value:0.7},
+      {axis:"Java",value:0.95},
+      {axis:"Python",value:0.65},
+      {axis:"C",value:0.5},
+      {axis:"Ionic",value:0.7},
+      {axis:"SQL",value:0.93},
+      {axis:"Software Architecture",value:0.7}
+    ]];
+let data2 = [[
+    {axis:"Italian",value:0.68},
+    {axis:"Spanish",value:0.95},
+    {axis:"English",value:0.82}
+    ]];
+let color = d3.scale.ordinal().range(["#31C69A"]);
+let color2 = d3.scale.ordinal().range(["#41748C"]);			
+let radarChartOptions = {
+    w: width,
+    h: height,
+    margin: {top: m, right: m, bottom: m, left: m},
+    maxValue: 1.0,
+    levels: 5,
+    roundStrokes: true,
+    color: color
+};
+let radarChartOptions2 = {
+    w: width,
+    h: height,
+    margin: {top: m, right: m, bottom: m, left: m},
+    maxValue: 1.0,
+    levels: 5,
+    roundStrokes: true,
+    color: color2
+};
+//Call function to draw the Radar chart
+RadarChart('.radarChart1', data, radarChartOptions);
+RadarChart('.radarChart2', data2, radarChartOptions2);
+
+function getAgent() {
+    window.innerWidth
+}
